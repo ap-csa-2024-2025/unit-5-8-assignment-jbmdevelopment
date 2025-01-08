e01afc7f9b11efd2f641bbfd8a05d804f5649394
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2025-01-08 19:04:07 +0000

    c

--- Dog.java
+++ Dog.java
@@ -3,14 +3,27 @@ public class Dog
   private String name;
   private int ID;
   private static int numDogs = 0;
-
   // Non-default Dog constructor
-
+  public Dog(String n) {
+    this.name = n;
+    numDogs++;
+    this.ID = numDogs;
+  }
   // Default Dog constructor
 
   // Setters - instance variables only
-
+  public void setName(String n) {
+    name = n;
+  }
+  public void setID(int id) {
+    ID = id;
+  }
   // Getters - instance variables only
-
+  public int getID() {
+    return ID;
+  }
+  public String getName() {
+    return name;
+  }
   // method getNumDogs
 }
