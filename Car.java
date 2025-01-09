ebb1c8cca860d43d6e50f27ab03865a518b4d3e6
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2025-01-09 19:10:41 +0000

    a

--- Car.java
+++ Car.java
@@ -1,4 +1,22 @@
 public class Car
 {
-  
+  private String make;
+  private String model;
+  private int year;
+  private double MPG; 
+  private static int ID;
+  public Car(String make, String model, int year, double mpg) {
+    this.make = make;
+    this.model = model
+    this.year = year;
+    this.mpg = mpg;
+    ID++;
+  }
+  public Car() {
+    this("None", "None", 2000, 0);
+  }
+  public String toString() {
+    String formatted = "ID: " + ID + "\n Make: " + this.make + "\n Model " + this.model + "\n Year: " + this.year + "\n MPG: " + this.mpg;
+    return formatted;
+  }
 }
